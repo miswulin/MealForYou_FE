3ac83903b5afd8c5bb6521afaec0ab64b91592dd
xeeone <xeeone@Jiwon-MacBook-Pro.local>
2025-11-29 18:21:47 +0900

    fix:pd빌드오류수정

--- src/api/dishes.js
+++ src/api/dishes.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const DISH_API_URL = "/api/dishes";
+const DISH_API_URL = "/api/dish";
 const CART_API_URL = "/api/cart";
 
 const authHeader = () => {
@@ -17,7 +17,7 @@ const authHeader = () => {
 export const dishesService = {
   getDishDetail: async (dishId) => {
     try {
-      const res = await axios.get(`${DETAIL_API_URL}/${dishId}`, {
+      const res = await axios.get(`${DISH_API_URL}/${dishId}`, {
         headers: authHeader(),
       });
       return res.data;
@@ -33,7 +33,7 @@ export const dishesService = {
 
   toggleInterest: async (dishId) => {
     const res = await axios.post(
-      `${DISHES_API_URL}/${dishId}/interest`,
+      `${DISH_API_URL}/${dishId}/interest`,
       null,
       { headers: authHeader() }
     );
@@ -42,13 +42,22 @@ export const dishesService = {
   },
 
 
-  addToCart: async (dishId, quantity, options) => {
-    const res = await axios.post(
-      CART_API_URL,
-      { dishId, quantity, options },
-      { headers: authHeader() }
-    );
-    return res.data;
+  addToCart: async (dishId, options) => {
+    try {
+      const res = await axios.post(
+        `${CART_API_URL}/add`, 
+        { dishId, options }, 
+        { headers: authHeader() }
+      );
+      return res.data;
+    } catch (error) {
+      console.error("장바구니 추가 오류:", error);
+      throw (
+        error.response?.data || {
+          message: "장바구니에 아이템 추가 중 오류가 발생했습니다.",
+        }
+      );
+    }
   },
 
   buyNow: async (dishId, options) => {
